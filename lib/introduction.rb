5b1387037fc42b92cdc488ac9c6a7e582f06e122
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-19 17:52:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(josh)
   puts "Hi, my name is Josh."
 end
 
-def introduction_with_language(dan, ember)
+def introduction_with_language(dan, ember.js)
   puts "Hi, my name is Dan and I am learning to prgram in Ember.js."
 end
